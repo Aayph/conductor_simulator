8b8c4df66138eb816cee809aa24159be7d965fbf
Aayph <mmehlitz@gmail.com>
2019-05-03 18:06:36 +0200

    spawning notes working

---
+++ data/music.lua
@@ -0,0 +1,13 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+
+
+minPitch = 70
+maxPitch = 86
+maxVelocity = 127
+
+songs = {}
+
+songs[0] = "0 On ch=1 n=72 v=100,480 Off ch=1 n=72 v=64,1920 On ch=1 n=72 v=100,2400 Off ch=1 n=72 v=64,3840 On ch=1 n=72 v=100,4320 Off ch=1 n=72 v=64,5760 On ch=1 n=72 v=100,6240 Off ch=1 n=72 v=64"
