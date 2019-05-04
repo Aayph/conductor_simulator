168b9ea3d0d3f1969690e5cbf38278f42b70a1a6
Dominik <mail.dominik.s@googlemail.com>
2019-05-04 17:36:51 +0200

    bad notes will be spawned

--- gameobjects/note_object.go
+++ gameobjects/note_object.go
@@ -33,3 +33,23 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "noteBG"
+  type: "sprite"
+  data: "tile_set: \"/assets/notes/notes.atlas\"\n"
+  "default_animation: \"note1_good\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
